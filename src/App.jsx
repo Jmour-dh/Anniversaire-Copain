07e56d331362b1d22f18beffe6d45b354eb0ec24
Jmour Dhia Eddine <125797700+Jmour-dh@users.noreply.github.com>
2023-09-29 12:45:51 +0200

    thrid commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
+import { useState } from "react";
+import data from "./data";
+import List from "./List";
 
 function App() {
- 
-
+  const [people, SetPeople] = useState(data);
+  console.log(people);
   return (
-   <h2>Rappel d&lsquo;anniversaire</h2>
-  )
+    <main>
+      <section className="container">
+        <h3>{people.length} Anniversaires d&lsquo;aujourd&lsquo;hui </h3>
+        <List people={people} />
+        <button type="button" className="btn btn-block" onClick={()=> SetPeople([])}>Effacer tous</button>
+      </section>
+    </main>
+  );
 }
 
-export default App
+export default App;
